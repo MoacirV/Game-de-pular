28666fcfb2cfa9c79d01625f02aa46f14bc82e49
Moacir Vieira <164024947+MoacirV@users.noreply.github.com>
2025-01-13 17:58:40 -0300

    star ajuste

--- Mm/script/script.js
+++ Mm/script/script.js
@@ -1,24 +1,34 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
-const jump = () =>{
+const jump = () => {
     mario.classList.add('jump');
 
-    setTimeout(() =>{
-        mario.classList.remove('jump')
+    setTimeout(() => {
+        mario.classList.remove('jump');
     }, 500);
-}
+};
 
-
-const loop = setInteval(() =>{
-
+const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    console.log(pipePosition)
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if(pipePosition <= 120){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        // Para a animação do cano
         pipe.style.animation = 'none';
-    }
+        pipe.style.left = `${pipePosition}px`;
 
-},10)
+        // Para a animação do Mario
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        // Substitui a imagem do Mario por uma de game over
+        mario.src = './assets/game-over.png';
+        mario.style.width = '100px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop); // Para o loop
+    }
+}, 10);
 
 document.addEventListener('keydown', jump);
